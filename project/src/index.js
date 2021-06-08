a3cfbc1921060d167b7b98978c83a9c11aba9c42
alex-home <sakun.alexey@gmail.com>
2021-06-08 21:52:57 +0300

    Create and pass offers mock

--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import offers from './mocks/offers';
 
 const Setting = {
   OFFERS_COUNT: 5,
@@ -8,6 +9,9 @@ const Setting = {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App offersCount={ Setting.OFFERS_COUNT }/>
+    <App
+      offersCount={Setting.OFFERS_COUNT}
+      offers={offers}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
