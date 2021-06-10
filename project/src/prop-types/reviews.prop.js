84a3fd84bbe731f8df840e09596b8df30e82d111
alex-home <sakun.alexey@gmail.com>
2021-06-10 22:23:17 +0300

    Create global prop types

---
+++ project/src/prop-types/reviews.prop.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+const reviewProp = PropTypes.shape({
+  comment: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date),
+  id: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    avatarUrl: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    isPro: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
+}).isRequired;
+
+const reviewsProp = PropTypes.arrayOf(reviewProp).isRequired;
+
+export { reviewProp, reviewsProp };
