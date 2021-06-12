ce3159c9210b06b5a670902cc79578c35f6caaac
alex-home <sakun.alexey@gmail.com>
2021-06-12 10:52:48 +0300

    Fill PlaceCard component with mock data

--- project/src/components/place-card/place-card.jsx
+++ project/src/components/place-card/place-card.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { offerProp } from '../../prop-types/offers.prop';
 import PlaceCardMark from '../place-card-mark/place-card-mark';
+import { capitalizeFirstLetter } from '../../utils/util';
+
+const MAX_RATING = 5;
+const PERCENTS_BASE = 100;
+
+function convertRatingToPercents(rating) {
+  return rating / MAX_RATING * PERCENTS_BASE;
+}
 
 function PlaceCard(props) {
   const { offer } = props;
@@ -10,13 +18,13 @@ function PlaceCard(props) {
       {offer.isPremium && <PlaceCardMark />}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width={260} height={200} alt="Place" />
+          <img className="place-card__image" src={offer.previewImage} width={260} height={200} alt="Place" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">€120</b>
+            <b className="place-card__price-value">€{offer.price}</b>
             <span className="place-card__price-text">/&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
@@ -28,14 +36,14 @@ function PlaceCard(props) {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: '80%'}} />
+            <span style={{width: `${convertRatingToPercents(offer.rating)}%`}} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{offer.title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{capitalizeFirstLetter(offer.type)}</p>
       </div>
     </article>
   );
