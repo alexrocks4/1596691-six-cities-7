287f5ced00192a4dbddde02f3d9023110e9a6eae
alex-home <sakun.alexey@gmail.com>
2021-06-12 17:28:26 +0300

    Add state to PlaceCardList

--- project/src/components/place-card-list/place-card-list.js
+++ project/src/components/place-card-list/place-card-list.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { offersProp } from '../../prop-types/offers.prop';
 import PlaceCard from '../place-card/place-card';
 
 function PlaceCardList({ offers }) {
-  return offers.map((offer) => <PlaceCard key={offer.id} offer={offer} />);
+  const [ activeOfferId, setActiveOfferId ] = useState(null);
+
+  function handleCardMouseEnter(id) {
+    setActiveOfferId(id);
+  }
+
+  function handleCardMouseLeave() {
+    setActiveOfferId(null);
+  }
+
+  return offers.map((offer) => (
+    <PlaceCard
+      key={offer.id}
+      offer={offer}
+      onCardMouseEnter={handleCardMouseEnter}
+      onCardMouseLeave={handleCardMouseLeave}
+    />
+  ));
 }
 
 PlaceCardList.propTypes = {
