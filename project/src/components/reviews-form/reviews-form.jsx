98664aca5c92f27f88406380635e1c1d53cc84fa
alex <sakun.alexey@gmail.com>
2021-06-15 17:53:53 +0300

    Create and render ReviewsForm component

---
+++ project/src/components/reviews-form/reviews-form.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { DECIMAL_RADIX } from '../../const';
+
+const DEFAULT_RATING = 0;
+const DEFAULT_REVIEW = '';
+const MIN_REVIEW_LENGTH = 50;
+const MAX_REVIEW_LENGTH = 300;
+
+function ReviewsForm() {
+  const [ rating, setRating ] = useState(DEFAULT_RATING);
+  const [ review, setReview ] = useState(DEFAULT_REVIEW);
+
+  function handleReviewChange(evt) {
+    setReview(evt.target.value);
+  }
+
+  function handleRatingChange(evt) {
+    setRating(Number.parseInt(evt.target.value, DECIMAL_RADIX));
+  }
+
+  const isReviewOk = review.length >= MIN_REVIEW_LENGTH && review.length < MAX_REVIEW_LENGTH;
+  const isSubmitDisbled = !isReviewOk || rating === DEFAULT_RATING;
+
+  return (
+    <form className="reviews__form form" action="#" method="post">
+      <label className="reviews__label form__label" htmlFor="review">Your review</label>
+      <div className="reviews__rating-form form__rating">
+        <input className="form__rating-input visually-hidden" name="rating" defaultValue={5} id="5-stars" type="radio" checked={rating === 5} onChange={handleRatingChange} />
+        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+        <input className="form__rating-input visually-hidden" name="rating" defaultValue={4} id="4-stars" type="radio" checked={rating === 4} onChange={handleRatingChange} />
+        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+        <input className="form__rating-input visually-hidden" name="rating" defaultValue={3} id="3-stars" type="radio" checked={rating === 3} onChange={handleRatingChange} />
+        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+        <input className="form__rating-input visually-hidden" name="rating" defaultValue={2} id="2-stars" type="radio" checked={rating === 2} onChange={handleRatingChange} />
+        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+        <input className="form__rating-input visually-hidden" name="rating" defaultValue={1} id="1-star" type="radio" checked={rating === 1} onChange={handleRatingChange} />
+        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+      </div>
+      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={review} onChange={handleReviewChange} />
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+        </p>
+        <button className="reviews__submit form__submit button" type="submit" disabled={isSubmitDisbled}>Submit</button>
+      </div>
+    </form>
+  );
+}
+
+export default ReviewsForm;
--- project/src/const.js
+++ project/src/const.js
@@ -1,7 +1,14 @@
-export const AppRoute = {
+const AppRoute = {
   MAIN: '/',
   LOGIN: '/login',
   FAVORITES: '/favorites',
   ROOM: '/offer/:id',
   ROOM_BASE: '/offer',
+};
+
+const DECIMAL_RADIX = 10;
+
+export {
+  AppRoute,
+  DECIMAL_RADIX
 };
