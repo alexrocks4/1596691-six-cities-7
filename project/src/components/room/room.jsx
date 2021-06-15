a070a9097725499570f2cf74ac3f5781ea8f78a8
alex <sakun.alexey@gmail.com>
2021-06-15 15:29:05 +0300

    Link Room component with other components

--- project/src/components/room/room.jsx
+++ project/src/components/room/room.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderLogo from '../header-logo/header-logo';
 import { reviewsProp } from '../../prop-types/reviews.prop';
 import { offersProp } from '../../prop-types/offers.prop';
@@ -9,6 +10,7 @@ import RoomMark from '../room-mark/room-mark';
 import Rating from '../rating/rating';
 import { capitalizeFirstLetter } from '../../utils/util';
 import ReviewsItem from '../reviews-item/reviews-item';
+import { AppRoute } from '../../const';
 
 function Room({ reviews, offers }) {
   const { id } = useParams();
@@ -37,16 +39,21 @@ function Room({ reviews, offers }) {
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
+                  <Link
+                    to={`${AppRoute.FAVORITES}`}
+                    className="header__nav-link header__nav-link--profile"
+                  >
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
+                  <Link
+                    to={`${AppRoute.LOGIN}`}
+                    className="header__nav-link"
+                  >
                     <span className="header__signout">Sign out</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </nav>
