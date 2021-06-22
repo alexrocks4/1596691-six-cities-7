0265e4d417f84bfc019d2545895359b2397bec3d
alex <sakun.alexey@gmail.com>
2021-06-22 17:03:49 +0300

    Add DevTools support

--- project/src/index.js
+++ project/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
 import { getFavoriteOffers } from './utils/util';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
