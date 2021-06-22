e30a2d0ff42c41bd1631e265a6d4053753b79bbe
alex <sakun.alexey@gmail.com>
2021-06-22 16:57:20 +0300

    Connect Redux store to app

--- project/src/index.js
+++ project/src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducer';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
 import { getFavoriteOffers } from './utils/util';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      offers={offers}
-      reviews={reviews}
-      favoriteOffers={getFavoriteOffers(offers)}
-    />
+    <Provider store={store}>
+      <App
+        offers={offers}
+        reviews={reviews}
+        favoriteOffers={getFavoriteOffers(offers)}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
