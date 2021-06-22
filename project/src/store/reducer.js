8e80d9bbffab5d4e2f829f529e90f35f667986e4
alex <sakun.alexey@gmail.com>
2021-06-22 12:59:19 +0300

    Add actions to reducer

--- project/src/store/reducer.js
+++ project/src/store/reducer.js
@@ -1,12 +1,23 @@
 import { CityName } from '../const';
+import { ActionType } from './action';
 
 const initialState = {
-  selectedCity: CityName.PARIS,
+  filterCity: CityName.PARIS,
   offers: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ActionType.SET_FILTER_CITY:
+      return {
+        ...state,
+        filterCity: action.payload,
+      };
+    case ActionType.SET_OFFERS:
+      return {
+        ...state,
+        offers: action.payload,
+      };
     default:
       return state;
   }
