7a8eb72c8afb58d29094a8e07dcce13f2d115e49
alex <sakun.alexey@gmail.com>
2021-07-12 14:21:19 +0300

    Link Favorites component with store using RTK hooks. WIP

--- project/src/store/api/selectors.js
+++ project/src/store/api/selectors.js
@@ -1,11 +1,33 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { NameSpace } from '../rootReducer';
 
 const selectOffers = (state) => state[NameSpace.API].offers.data;
 const selectOffersStatus = (state) => state[NameSpace.API].offers.status;
 const selectOffersError = (state) => state[NameSpace.API].offers.error;
+const selectFavoriteOffers = createSelector(
+  selectOffers,
+  (offers) => offers.filter((offer) => offer.isFavorite),
+);
+const selectFavoriteOffersGroupedByCities = createSelector(
+  selectFavoriteOffers,
+  (offers) => {
+    const groupedOffers = new Map();
+
+    offers.forEach((offer) => {
+      const { city: { name: cityName} } = offer;
+      const oldOffers = groupedOffers.get(cityName);
+      const newOffers = oldOffers ? [ ...oldOffers, offer ] : [ offer ];
+
+      groupedOffers.set(cityName, newOffers);
+    });
+
+    return groupedOffers;
+  },
+);
 
 export {
   selectOffers,
   selectOffersStatus,
-  selectOffersError
+  selectOffersError,
+  selectFavoriteOffersGroupedByCities
 };
