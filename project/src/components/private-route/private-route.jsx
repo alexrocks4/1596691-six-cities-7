9a53e43db5b7fb2cc4f0dacd4d8972fefbf321ff
alex <sakun.alexey@gmail.com>
2021-07-12 16:46:36 +0300

    Link PrivateRoute component with store using RTK hooks. WIP

--- project/src/components/private-route/private-route.jsx
+++ project/src/components/private-route/private-route.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { AppRoute } from '../../const';
-import { isAuthorized } from '../../utils/util';
+import { selectIsUserAuthorized } from '../../store/user/selectors';
 
 function PrivateRoute(props) {
   const {
     children,
     path,
     exact,
-    isUserAuthorized,
     unauthorizedContent,
   } = props;
+
+  const isUserAuthorized = useSelector(selectIsUserAuthorized);
 
   return (
     <Route
@@ -26,7 +27,6 @@ function PrivateRoute(props) {
 
 PrivateRoute.propTypes = {
   children: PropTypes.node.isRequired,
-  isUserAuthorized: PropTypes.bool.isRequired,
   exact: PropTypes.bool.isRequired,
   path: PropTypes.string.isRequired,
   unauthorizedContent: PropTypes.node.isRequired,
@@ -36,10 +36,5 @@ PrivateRoute.defaultProps = {
   unauthorizedContent: <Redirect to={AppRoute.LOGIN} />,
 };
 
-const mapStateToProps = (state) => ({
-  isUserAuthorized: isAuthorized(state.authorizationStatus),
-});
-
-
 export {PrivateRoute};
-export default connect(mapStateToProps)(PrivateRoute);
+export default PrivateRoute;
