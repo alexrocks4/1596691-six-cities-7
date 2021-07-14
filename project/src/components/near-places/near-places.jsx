d0f2b535b6e0c0a0156a756b56b117c528c3d9ad
alex <sakun.alexey@gmail.com>
2021-07-14 16:20:06 +0300

    Reuse PlaceCardList in NearPlacesComponent

--- project/src/components/near-places/near-places.jsx
+++ project/src/components/near-places/near-places.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { offersProp } from '../../prop-types/offers.prop';
+import PlaceCardList from '../place-card-list/place-card-list';
 import PlaceCardRoom from '../place-card-room/place-card-room';
 
 function NearPlaces({ offers }) {
   return (
     <section className="near-places places">
       <h2 className="near-places__title">Other places in the neighbourhood</h2>
-      <div className="near-places__list places__list">
-        {offers.map((offer) => (
+      <PlaceCardList
+        className="near-places__list places__list"
+        offers={offers}
+        render={(offer) => (
           <PlaceCardRoom
             key={offer.id}
             offer={offer}
           />
-        ))}
-      </div>
+        )}
+      />
     </section>
   );
 }
