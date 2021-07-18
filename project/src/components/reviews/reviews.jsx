b57cbce3538455ce8f84676947cf574bfc248d20
alex-home <sakun.alexey@gmail.com>
2021-07-18 20:40:36 +0300

    Hide review form if user is not authorized

--- project/src/components/reviews/reviews.jsx
+++ project/src/components/reviews/reviews.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectReviews } from '../../store/api/selectors';
+import { selectIsUserAuthorized } from '../../store/user/selectors';
 import ReviewsForm from '../reviews-form/reviews-form';
 import ReviewsItem from '../reviews-item/reviews-item';
 
 function Reviews() {
   const reviews = useSelector(selectReviews);
+  const isUserAuthorized = useSelector(selectIsUserAuthorized);
 
   return (
     <section className="property__reviews reviews">
@@ -13,7 +15,7 @@ function Reviews() {
       <ul className="reviews__list">
         {reviews.map((review) => <ReviewsItem key={review.id} review={review} />)}
       </ul>
-      <ReviewsForm />
+      {isUserAuthorized && <ReviewsForm />}
     </section>
   );
 }
