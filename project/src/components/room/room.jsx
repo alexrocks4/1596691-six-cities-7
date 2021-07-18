cd5bc2c79c0b70e3a65ad0a7e9cc9c7f38c9c66a
alex-home <sakun.alexey@gmail.com>
2021-07-18 14:18:14 +0300

    Implement api errors render logic in Room component

--- project/src/components/room/room.jsx
+++ project/src/components/room/room.jsx
@@ -4,11 +4,18 @@ import { reviewsProp } from '../../prop-types/reviews.prop';
 import { useParams } from 'react-router-dom';
 import NotFound from '../not-found/not-found';
 import Header from '../header/header';
-import { selectIsOfferFetchingFailed, selectIsOfferLoading } from '../../store/api/selectors';
+import {
+  selectIsOfferFetchingFailed,
+  selectIsOfferLoading,
+  selectIsOfferNotFound,
+  selectOfferErrorStatusCode,
+  selectOfferErrorStatusText
+} from '../../store/api/selectors';
 import { fetchNearbyOffers, fetchOffer } from '../../store/api-actions';
 import { DECIMAL_RADIX } from '../../const';
 import Loading from '../loading/loading';
 import RoomMainContent from '../room-main-content/room-main-content';
+import Error from '../error/error';
 
 function Room({ reviews }) {
   let { id } = useParams();
@@ -17,14 +24,24 @@ function Room({ reviews }) {
   const dispatch = useDispatch();
   const isOfferLoading = useSelector(selectIsOfferLoading);
   const isOfferFetchingFailed = useSelector(selectIsOfferFetchingFailed);
+  const isOfferNotFound = useSelector(selectIsOfferNotFound);
+  const offerErrorStatusCode = useSelector(selectOfferErrorStatusCode);
+  const offerErrorStatusText = useSelector(selectOfferErrorStatusText);
 
   useEffect(() => {
     dispatch(fetchOffer(id));
     dispatch(fetchNearbyOffers(id));
   }, [dispatch, id]);
 
-  if (isOfferFetchingFailed) {
-    return <NotFound />;
+  if (isOfferNotFound) {
+    return <NotFound statusCode={offerErrorStatusCode}/>;
+  } else if (isOfferFetchingFailed) {
+    return (
+      <Error
+        message={offerErrorStatusText}
+        statusCode={offerErrorStatusCode}
+      />
+    );
   }
 
   return (
