91e6094c773a497a8c5994d78f9487a098f73521
alex <sakun.alexey@gmail.com>
2021-07-19 16:35:05 +0300

    Use current city name in CitiesNoPlaces component

--- project/src/components/cities-no-places/cities-no-places.jsx
+++ project/src/components/cities-no-places/cities-no-places.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectCity } from '../../store/app/selectors';
 
 function CitiesNoPlaces() {
+  const currentCity = useSelector(selectCity);
+
   return (
     <React.Fragment>
       <section className="cities__no-places">
         <div className="cities__status-wrapper tabs__content">
           <b className="cities__status">No places to stay available</b>
-          <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+          <p className="cities__status-description">We could not find any property available at the moment in {currentCity}</p>
         </div>
       </section>
       <div className="cities__right-section" />
