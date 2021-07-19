d2aa6a4e16b3baa75fa16a7af7f031f17b1b46ff
alex <sakun.alexey@gmail.com>
2021-07-19 15:09:24 +0300

    Render conditionally CitiesNoPlaces component

--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -5,6 +5,8 @@ import Header from '../header/header';
 import { selectCity } from '../../store/app/selectors';
 import { makeSelectFilteredOffersByCity, makeSelectSortedOffers } from '../../store/api/selectors';
 import CitiesPlaces from '../cities-places/cities-places';
+import CitiesNoPlaces from '../cities-no-places/cities-no-places';
+import classNames from 'classnames';
 
 function Main() {
   const selectFilteredOffersByCity = useMemo(makeSelectFilteredOffersByCity, []);
@@ -12,15 +14,36 @@ function Main() {
   const currentCity = useSelector(selectCity);
   const filteredOffersByCity = useSelector((state) => selectFilteredOffersByCity(state, currentCity));
   const sortedOffers = useSelector((state) => selectSortedOffers(state, filteredOffersByCity));
+  const isNoOffers = !sortedOffers.length;
 
   const [ activeOffer, setActiveOffer ] = useState(null);
   const handlePlacesCardMouseEnter = useCallback((offer) => setActiveOffer(offer), []);
   const handleLocationClick = useCallback(() => setActiveOffer(null), []);
 
+  let placesContent;
+
+  if (isNoOffers) {
+    placesContent = <CitiesNoPlaces />;
+  }
+  else {
+    placesContent = (
+      <CitiesPlaces
+        offers={sortedOffers}
+        currentCity={currentCity}
+        activeOffer={activeOffer}
+        onPlacesCardMouseEnter={handlePlacesCardMouseEnter}
+      />
+    );
+  }
+
   return (
     <div className="page page--gray page--main">
       <Header isLogoLinkActive />
-      <main className="page__main page__main--index">
+      <main className={classNames(
+        'page__main page__main--index',
+        { 'page__main--index-empty': isNoOffers },
+      )}
+      >
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -28,13 +51,12 @@ function Main() {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <CitiesPlaces
-              offers={sortedOffers}
-              currentCity={currentCity}
-              activeOffer={activeOffer}
-              onPlacesCardMouseEnter={handlePlacesCardMouseEnter}
-            />
+          <div className={classNames(
+            'cities__places-container container',
+            { 'cities__places-container--empty': isNoOffers },
+          )}
+          >
+            {placesContent}
           </div>
         </div>
       </main>
