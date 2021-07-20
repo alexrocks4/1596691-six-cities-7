b53436fb211e7ad5a7e6099dd72b8832d1e0d26a
alex-home <sakun.alexey@gmail.com>
2021-07-20 20:29:08 +0300

    Implement unauthorized user redirection to SignIn on BookmarkButton click

--- project/src/components/bookmark-button/bookmark-button.jsx
+++ project/src/components/bookmark-button/bookmark-button.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { offerProp } from '../../prop-types/offers.prop';
 import { setOfferAsFavorite, unsetOfferAsFavorite } from '../../store/api-actions';
+import { selectIsUserAuthorized } from '../../store/user/selectors';
+import { redirectedToRoute } from '../../store/action';
+import { APIRoute } from '../../const';
 
 const DEFAULT_WIDTH = 18;
 const DEFAULT_HEIGHT = 19;
 
 function BookmarkButton({ offer, onClick, dimension }) {
   const dispatch = useDispatch();
+  const isUserAuthorized = useSelector(selectIsUserAuthorized);
 
   const handleButtonClick = () => {
+    if (!isUserAuthorized) {
+      dispatch(redirectedToRoute(APIRoute.LOGIN));
+      return;
+    }
+
     if (offer.isFavorite) {
       dispatch(unsetOfferAsFavorite(offer.id, onClick));
       return;
