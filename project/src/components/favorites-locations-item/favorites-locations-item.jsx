8defd36e68b92a3e5d704259945fedb21ca7d15e
alex-home <sakun.alexey@gmail.com>
2021-07-27 20:01:49 +0300

    Fix eslint warning - href attribute requires a valid value

--- project/src/components/favorites-locations-item/favorites-locations-item.jsx
+++ project/src/components/favorites-locations-item/favorites-locations-item.jsx
@@ -8,7 +8,11 @@ function FavoritesLocationsItem({ cityName, offers }) {
     <li className="favorites__locations-items">
       <div className="favorites__locations locations locations--current">
         <div className="locations__item">
-          <a className="locations__item-link" href="#">
+          <a
+            className="locations__item-link"
+            href={`#${cityName.toLowerCase()}`}
+            onClick={(evt) => evt.preventDefault()}
+          >
             <span>{cityName}</span>
           </a>
         </div>
